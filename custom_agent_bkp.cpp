403bcb24f5a6778a368ed09d1dad143389cd8c35
Sorokonog <47176637+Sorokonog@users.noreply.github.com>
2022-11-10 11:58:38 +0300

    Create custom_agent_bkp.cpp

---
+++ custom_agent_bkp.cpp
@@ -0,0 +1,253 @@
+#include <uxr/agent/transport/custom/CustomAgent.hpp>
+#include <uxr/agent/transport/endpoint/CustomEndPoint.hpp>
+
+#include <sys/time.h>
+
+#include <poll.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <signal.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include <uxr/agent/transport/can/CanAgentLinux.hpp>
+#include <uxr/agent/utils/Conversion.hpp>
+#include <uxr/agent/logger/Logger.hpp>
+
+#include <net/if.h>
+#include <sys/ioctl.h>
+
+#include <linux/can.h>
+#include <linux/can/raw.h>
+
+/**
+ * This custom XRCE Agent example attempts to show how easy is for the user to define a custom
+ * Micro XRCE-DDS Agent behaviour, in terms of transport initialization and closing, and also
+ * regarding read and write operations.
+ * For this simple case, an UDP socket is opened on port 8888. Additionally, some information
+ * messages are being printed to demonstrate the custom behaviour.
+ * As the endpoint is already defined, we are using the provided
+ * `eprosima::uxr::IPv4EndPoint` by the library.
+ * Other transport protocols might need to implement their own endpoint struct.
+ */
+int64_t time_ms(void);
+
+int main(int argc, char** argv)
+{
+    eprosima::uxr::Middleware::Kind mw_kind(eprosima::uxr::Middleware::Kind::FASTDDS);
+    
+    struct pollfd poll_fd;
+    struct sockaddr_can addr;
+    struct ifreq ifr;
+
+    /**
+     * @brief Agent's initialization behaviour description.
+     */
+    eprosima::uxr::CustomAgent::InitFunction init_function = [&]() -> bool
+    {
+        struct canfd_frame frame;
+        struct can_frame rec_frame;
+
+        bool rv = false;
+
+        if ((poll_fd.fd = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) 
+        {
+		perror("socket creation: ");
+		return false;
+        }
+        
+        if (-1 != poll_fd.fd)
+        {
+            struct sockaddr_in address{};
+            strcpy(ifr.ifr_name, "can0");
+            ioctl(poll_fd.fd, SIOCGIFINDEX, &ifr);
+            poll_fd.events = POLLIN;
+            addr.can_family  = AF_CAN;
+            addr.can_ifindex = ifr.ifr_ifindex;
+            if (-1 != bind(poll_fd.fd,
+                (struct sockaddr *)&addr,
+                sizeof(addr)))
+                {
+                    rv = true;
+                }
+            else
+            {
+                perror("socket bind: ");
+            }
+        }
+            {
+                rv = true;
+                UXR_AGENT_LOG_INFO(
+                    UXR_DECORATE_GREEN(
+                        "This is an example of a custom Micro XRCE-DDS Agent INIT function"),
+                    "fd: {}",
+                    poll_fd.fd);
+            }
+        return rv;
+    };
+
+    /**
+     * @brief Agent's destruction actions.
+     */
+    eprosima::uxr::CustomAgent::FiniFunction fini_function = [&]() -> bool
+    {
+        if (-1 == poll_fd.fd)
+        {
+            return true;
+        }
+
+        if (0 == close(poll_fd.fd))
+        {
+            poll_fd.fd = -1;
+
+            UXR_AGENT_LOG_INFO(
+                UXR_DECORATE_GREEN(
+                    "This is an example of a custom Micro XRCE-DDS Agent FINI function"),
+                "fd: {}",
+                poll_fd.fd);
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    };
+
+    /**
+     * @brief Agent's incoming data functionality.
+     */
+    eprosima::uxr::CustomAgent::RecvMsgFunction recv_msg_function = [&](
+            eprosima::uxr::CustomEndPoint* source_endpoint,
+            uint8_t* buffer,
+            size_t buffer_length,
+            int timeout,
+            eprosima::uxr::TransportRc& transport_rc) -> ssize_t
+    {
+        struct can_frame frame;
+        uint16_t rv;
+        rv = 0;
+        int64_t start = time_ms();
+
+        while ((time_ms() -  start) < timeout || rv > 0)
+	    {
+            rv = poll(&poll_fd, 1, 10);
+                if (rv > 0)
+                {
+                    read(poll_fd.fd,&frame,sizeof(struct can_frame));
+                    memcpy(buffer,&(frame.data),frame.can_dlc);
+                    source_endpoint->set_member_value<uint16_t>("ID",frame.can_id);
+                    transport_rc = (-1 != rv)
+                    ? eprosima::uxr::TransportRc::ok
+                    : eprosima::uxr::TransportRc::server_error;
+                    return frame.can_dlc;
+                 }
+        }
+        transport_rc = eprosima::uxr::TransportRc::timeout_error;
+        return -1;
+    };
+
+    /**
+     * @brief Agent's outcoming data flow definition.
+     */
+    eprosima::uxr::CustomAgent::SendMsgFunction send_msg_function = [&](
+        const eprosima::uxr::CustomEndPoint* destination_endpoint,
+        uint8_t* buffer,
+        size_t message_length,
+        eprosima::uxr::TransportRc& transport_rc) -> ssize_t
+    {
+        struct can_frame frame;
+        uint8_t * ptr = buffer;
+        int rv;
+
+        ssize_t bytes_sent = 0;
+        frame.can_id = destination_endpoint->get_member<uint16_t>("ID") + 10;
+        frame.can_dlc = 8;
+
+        uint16_t cycle, rest;
+        
+        cycle = message_length / frame.can_dlc;
+        rest = message_length % frame.can_dlc;
+
+        for(cycle; cycle>0; cycle--)
+        {
+            memcpy(&(frame.data),ptr,8);
+            rv = write(poll_fd.fd, &frame, 16);
+            if (rv != -1)
+            {
+                bytes_sent += 8;
+            }
+            ptr += 8;
+        }
+        if (rest != 0)
+        {
+            frame.can_dlc = rest;
+            memcpy(&(frame.data), ptr, rest);
+            rv = write(poll_fd.fd, &frame,  16);
+            if (rv != -1)
+            {
+                bytes_sent += rest;
+            }    
+        }
+        transport_rc = (-1 != rv)
+        ? eprosima::uxr::TransportRc::ok
+        : eprosima::uxr::TransportRc::server_error;
+        return bytes_sent;
+    };
+
+    /**
+     * Run the main application.
+     */
+    try
+    {
+        /**
+         * EndPoint definition for this transport. We define an address and a port.
+         */
+        eprosima::uxr::CustomEndPoint custom_endpoint;
+        custom_endpoint.add_member<uint16_t>("ID");
+        /**
+         * Create a custom agent instance.
+         */
+        eprosima::uxr::CustomAgent custom_agent(
+            "CAN_BUS",
+            &custom_endpoint,
+            mw_kind,
+            true,
+            init_function,
+            fini_function,
+            send_msg_function,
+            recv_msg_function);
+
+        /**
+         * Set verbosity level
+         */
+        custom_agent.set_verbose_level(0);
+
+        /**
+         * Run agent and wait until receiving an stop signal.
+         */
+        custom_agent.start();
+
+        int n_signal = 0;
+        sigset_t signals;
+        sigwait(&signals, &n_signal);
+
+        /**
+         * Stop agent, and exit.
+         */
+        custom_agent.stop();
+        return 0;
+    }
+    catch (const std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+        return 1;
+    }
+}
+
+int64_t time_ms(void)
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return (int64_t)tv.tv_sec * 1000 + tv.tv_usec / 1000;
+}
